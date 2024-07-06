d4e88faa84e75f28feb9b53956d84d25dcb6cc2c
Sayan Sarkar <93654533+Sayan-sarkar-2k03@users.noreply.github.com>
2024-07-06 14:28:41 +0530

    Add files via upload

---
+++ assets/portfolio1.js
@@ -0,0 +1,6 @@
+var typed=new Typed(".auto-type", {
+    strings: ["Frontend Web Developer","Ethical Hacker","Designer","java Developer"],
+    typeSpeed : 100,
+    backSpeed : 60,
+    loop : true
+  });
