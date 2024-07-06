50dcc8ae1c65f8478217d8bdf8c20af83e884a7d
Sayan Sarkar <93654533+Sayan-sarkar-2k03@users.noreply.github.com>
2024-07-06 14:25:42 +0530

    Delete portfolio1.js

--- portfolio1.js
+++
@@ -1,6 +0,0 @@
-var typed=new Typed(".auto-type", {
-    strings: ["Frontend Web Developer","Ethical Hacker","Designer","java Developer"],
-    typeSpeed : 100,
-    backSpeed : 60,
-    loop : true
-  });
